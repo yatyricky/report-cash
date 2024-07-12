0091d3c57e2baa4b6eeac1e8de4412cf1fe85657
Rick Sun <yatyricky@gmail.com>
2024-07-12 23:33:16 +0800

    1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require("fs")
 const xlsx = require("xlsx")
 const { utils } = xlsx
+const XlsxPopulate = require('xlsx-populate');
+
+const KVArray = require("./KVArray.js")
 
 const config = JSON.parse(fs.readFileSync("config.json", "utf8"))
 
@@ -53,132 +56,109 @@ function padWith0(inStr, len) {
     return str
 }
 
-function GetOrCreateObject(obj, key) {
-    let o = obj[key]
-    if (o === undefined) {
-        const n = Object.keys(obj).length
-        o = { sort: n, sum: 0 }
-        obj[key] = o
+const KW_INCOME = "收入"
+const KW_EXPENDITURE = "支出"
+
+function newAccountData() {
+    return {
+        data: new KVArray(),
+        current: 0,
+        total: 0
     }
-    return o
 }
 
-function ObjectToArray(obj) {
-    const arr = []
-    for (const key in obj) {
-        if (Object.hasOwnProperty.call(obj, key)) {
-            const value = obj[key];
-            arr.push({ _k: key, _v: value })
+const report = newAccountData()
+report.data.add(newAccountData(), KW_INCOME)
+report.data.add(newAccountData(), KW_EXPENDITURE)
+
+async function program() {
+    // const outWb = await XlsxPopulate.fromBlankAsync()
+    // const outWb = utils.book_new()
+
+    function flushReport(moonTag) {
+        const newSheet = [
+            [`个人借支明细账${moonTag}`],
+            ["科目", "类别明细", "序号", "内容", "本期发生", "累计金额", "备注"],
+        ]
+
+        for (let i = 0; i < report.data.size(); i++) {
+            const acc1 = report.data.getByIndex(i);
+            for (let j = 0; j < acc1.data.size(); j++) {
+                const acc2 = acc1.data.getByIndex(j);
+                const row = [null, null, 0, null, 0, 0, ""]
+                for (let k = 0; k < acc2.data.size(); k++) {
+                    const entry = acc2.data.getByIndex(k);
+                    if (k === 0) {
+                        row[1] = acc1.data.getKey(j)
+                        if (j === 0) {
+                            row[0] = report.data.getKey(i)
+                        }
+                    }
+                    row[2] = k + 1
+                    row[3] = acc2.data.getKey(k)
+                    row[4] = entry.current
+                    row[5] = entry.total
+                }
+                newSheet.push(row)
+            }
+        }
+        for (const row of newSheet) {
+            console.log(row.join(","));
         }
     }
-    arr.sort((a, b) => {
-        return b.sort - a.sort
-    })
-    return arr
-}
 
-const report = {
-    income: {
-        ["收入"]: { sort: 0, sum: 0 } // key=account, value={sort:1, current:12, total:100}
-    },
-    outcome: {}, // key=secondary, value={bulk}
-}
-
-const outWb = utils.book_new()
-const outSheets = []
-
-function flushReport(moonTag) {
-    const newSheet = [
-        [`孙世龙个人借支明细账${moonTag}`],
-        ["科目", "类别明细", "序号", "内容", "本期发生", "累计金额", "备注"],
-    ]
-
-    // income
-    const first = ObjectToArray(report.income)
-    for (let i = 0; i < first.length; i++) {
-        const data = first[i];
-        const second = ObjectToArray(data)
-        for (let j = 0; j < second.length; j++) {
-            const entry = second[j];
-            const row = []
-            if (i === 0) {
-                row.push("收入")
-            } else {
-                row.push(null)
-            }
-            if (i === 0) {
-                row.push(data._k)
-            } else {
-                row.push(null)
-            }
-            row.push(j + 1)
-            row.push()
+    const range = utils.decode_range(ws["!ref"])
+    let currentMonth
+    for (let r = 1; r <= range.e.r; r++) {
+        const account = GetString(ws, r, 3).trim()
+        if (account.length === 0) {
+            continue
         }
 
+        const date = GetDate(ws, r, 0)
+
+        const moonTag = `${date.getFullYear()}-${padWith0(date.getMonth() + 1, 2)}`
+        if (currentMonth !== undefined && moonTag !== currentMonth) {
+            flushReport(moonTag)
+        }
+        currentMonth = moonTag
+
+        const desc = GetString(ws, r, 1)
+        const debit = GetDouble(ws, r, 4)
+        const credit = GetDouble(ws, r, 5)
+
+        const tokens = account.split("_")
+        let acc1
+        const acc2 = tokens[0]
+        const acc3 = tokens.slice(1).join("_")
+
+        if (tokens[0] === KW_INCOME) {
+            acc1 = KW_INCOME
+        } else {
+            acc1 = KW_EXPENDITURE
+        }
+
+        const tab1 = report.data.getByKey(acc1, newAccountData)
+        const tab = tab1.data.getByKey(acc2, newAccountData)
+        const entry = tab.data.getByKey(acc3, () => {
+            return { current: 0, total: 0 }
+        })
+
+        const delta = debit - credit
+        entry.current += delta
+        entry.total += delta
+        tab.current += delta
+        tab.total += delta
+        tab1.current += delta
+        tab1.total += delta
+        report.current += delta
+        report.total += delta
     }
 
-    const ws = utils.aoa_to_sheet(newSheet)
-    ws["!merges"] = [
-        { s: { r: 0, c: 0 }, e: { r: 0, c: 6 } }
-        // { s: { r: 2, c: 0 }, e: { r: 5, c: 0 } }
-    ]
-
-    outSheets.push({ ws, name: moonTag })
+    flushReport(currentMonth)
 }
 
-const range = utils.decode_range(ws["!ref"])
-let currentMonth
-for (let r = 1; r <= range.e.r; r++) {
-    const account = GetString(ws, r, 3).trim()
-    if (account.length === 0) {
-        continue
-    }
-
-    const date = GetDate(ws, r, 0)
-
-    const moonTag = `${date.getFullYear()}-${padWith0(date.getMonth() + 1, 2)}`
-    if (currentMonth !== undefined && moonTag !== currentMonth) {
-        flushReport(moonTag)
-    }
-    currentMonth = moonTag
-
-    const desc = GetString(ws, r, 1)
-    const debit = GetDouble(ws, r, 4)
-    const credit = GetDouble(ws, r, 5)
-
-    const tokens = account.split("_")
-    const acc1 = tokens[0]
-    const acc2 = tokens.slice(1).join("_")
-
-    let tab
-    if (acc1 === "收入") {
-        tab = report.income.income
-    } else {
-        tab = GetOrCreateObject(report.outcome, acc1)
-    }
-
-    const entry = GetOrCreateObject(tab, acc2)
-    if (entry.current === undefined) {
-        entry.current = 0
-    }
-    if (entry.total === undefined) {
-        entry.total = 0
-    }
-
-    const delta = debit - credit
-    entry.current += delta
-    entry.total += delta
-    tab.sum += delta
-}
-
-flushReport(currentMonth)
-
-for (let i = outSheets.length - 1; i >= 0; i--) {
-    const data = outSheets[i]
-    utils.book_append_sheet(outWb, data.ws, data.name, true)
-}
-
-
-xlsx.writeFileXLSX(outWb, "test.xlsx")
-
-console.log(ws.A1.s);
+program().catch(e => {
+    console.log("FATAL ERROR OCCURRED");
+    console.log(e);
+})
