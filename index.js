31a2f0bbd32b461aa1b3baaed1e54c8a5ad6e75c
Rick Sun <yatyricky@gmail.com>
2024-07-13 23:46:56 +0800

    ms

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const path = require("path")
 const xlsx = require("xlsx")
 const { utils } = xlsx
 const XlsxPopulate = require('xlsx-populate');
@@ -56,6 +57,10 @@ function padWith0(inStr, len) {
     return str
 }
 
+function round(n) {
+    return Math.round(n * 100) / 100
+}
+
 const KW_INCOME = "收入"
 const KW_EXPENDITURE = "支出"
 
@@ -72,20 +77,28 @@ async function program() {
     report.data.add(KW_INCOME, newAccountData())
     report.data.add(KW_EXPENDITURE, newAccountData())
 
-    // const outWb = await XlsxPopulate.fromBlankAsync()
-    // const outWb = utils.book_new()
+    const outWb = await XlsxPopulate.fromBlankAsync()
 
     function flushReport(moonTag) {
-        const newSheet = [
-            [`个人借支明细账${moonTag}`],
-            ["科目", "类别明细", "序号", "内容", "本期发生", "累计金额", "备注"],
-        ]
+        const outWs = outWb.addSheet(moonTag, 0)
+        outWs.cell("A1").value(`个人借支明细账${moonTag}`)
+        outWs.column("B").width(12)
+        outWs.column("C").width(6)
+        outWs.column("D").width(30)
+        outWs.column("E").width(15).style({ numberFormat: "0.00" })
+        outWs.column("F").width(15).style({ numberFormat: "0.00" })
+        outWs.range("A1:G1").merged(true).style({ horizontalAlignment: "center", fontSize: 16 })
+        outWs.range("A2:G2").value([["科目", "类别明细", "序号", "内容", "本期发生", "累计金额", "备注"]]).style({ bold: true })
+        let r = 3
 
         for (let i = 0; i < report.data.size(); i++) {
             const [acc1Key, acc1Value] = report.data.getKVPair(i);
+            const credit = acc1Key === KW_EXPENDITURE ? -1 : 1
+            let rowSpan = 0
             for (let j = 0; j < acc1Value.data.size(); j++) {
                 const [acc2Key, acc2Value] = acc1Value.data.getKVPair(j);
-                for (let k = 0; k < acc2Value.data.size(); k++) {
+                const acc2Len = acc2Value.data.size()
+                for (let k = 0; k < acc2Len; k++) {
                     const row = [null, null, 0, null, 0, 0, ""]
                     const [entryKey, entryValue] = acc2Value.data.getKVPair(k);
                     if (k === 0) {
@@ -96,14 +109,47 @@ async function program() {
                     }
                     row[2] = k + 1
                     row[3] = entryKey
-                    row[4] = entryValue.current
-                    row[5] = entryValue.total
-                    newSheet.push(row)
+                    row[4] = round(entryValue.current * credit)
+                    row[5] = round(entryValue.total * credit)
+                    outWs.range(`A${r}:G${r}`).value([row])
+                    r++
+                }
+                outWs.range(`B${r - acc2Len}:B${r - 1}`).merged(true).style({ verticalAlignment: "center" })
+                const rowSubtotal = [null, "小计", null, null, round(acc2Value.current * credit), round(acc2Value.total * credit)]
+                outWs.range(`A${r}:G${r}`).value([rowSubtotal])
+                outWs.range(`B${r}:D${r}`).merged(true)
+                outWs.range(`B${r}:G${r}`).style({ fill: "DDDDDD" })
+                r++
+
+                rowSpan += acc2Len + 1
+            }
+            const rowGrandTotal = ["合计", null, null, null, round(acc1Value.current * credit), round(acc1Value.total * credit)]
+            outWs.range(`A${r}:G${r}`).value([rowGrandTotal]).style({ fill: "BBBBBB" })
+            outWs.range(`A${r}:D${r}`).merged(true)
+            if (rowSpan > 0) {
+                outWs.range(`A${r - rowSpan}:A${r - 1}`).merged(true).style({ verticalAlignment: "center" })
+            }
+            r++
+        }
+        const rowBalance = ["余额", null, null, null, round(report.current), round(report.total)]
+        outWs.range(`A${r}:G${r}`).value([rowBalance]).style({ bold: true })
+        outWs.range(`A${r}:D${r}`).merged(true)
+
+        outWs.range(`A2:G${r}`).style({ borderStyle: "thin" })
+
+        // clear current
+        report.current = 0
+        for (let i = 0; i < report.data.size(); i++) {
+            const [_1, acc1Value] = report.data.getKVPair(i);
+            acc1Value.current = 0
+            for (let j = 0; j < acc1Value.data.size(); j++) {
+                const [_2, acc2Value] = acc1Value.data.getKVPair(j);
+                acc2Value.current = 0
+                for (let k = 0; k < acc2Value.data.size(); k++) {
+                    const [_3, entryValue] = acc2Value.data.getKVPair(k);
+                    entryValue.current = 0
                 }
             }
-        }
-        for (const row of newSheet) {
-            console.log(row.join(","));
         }
     }
 
@@ -118,8 +164,11 @@ async function program() {
         const date = GetDate(ws, r, 0)
 
         const moonTag = `${date.getFullYear()}-${padWith0(date.getMonth() + 1, 2)}`
-        if (currentMonth !== undefined && moonTag !== currentMonth) {
-            flushReport(moonTag)
+        if (currentMonth === undefined) {
+            currentMonth = moonTag
+        }
+        if (moonTag !== currentMonth) {
+            flushReport(currentMonth)
         }
         currentMonth = moonTag
 
@@ -156,6 +205,11 @@ async function program() {
     }
 
     flushReport(currentMonth)
+    outWb.deleteSheet("Sheet1")
+    outWb.activeSheet(currentMonth)
+
+    const outFilePath = `${path.parse(config.filePath).name}报表-${currentMonth}.xlsx`
+    await outWb.toFileAsync(outFilePath)
 }
 
 program().catch(e => {
